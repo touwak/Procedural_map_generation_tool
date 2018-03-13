fae1b14a8aeb9aa61d43f6f370c88d6c21cf376f
touwak <claramuntgi@esat-alumni.com>
2018-03-13 17:30:37 +0000

    blend cell colors

--- Assets/Scripts/Hexagons/HexMetrics.cs
+++ Assets/Scripts/Hexagons/HexMetrics.cs
@@ -7,6 +7,9 @@ public class HexMetrics {
   public const float outerRadius = 10f;
   public const float innerRadius = outerRadius * 0.866025404f;
 
+  public const float solidFactor = 0.75f;
+  public const float blendFactor = 1f - solidFactor;
+
   public static Vector3[] corners = {
     new Vector3(0f, 0f, outerRadius),
     new Vector3(innerRadius, 0f, 0.5f * outerRadius),
@@ -16,5 +19,24 @@ public class HexMetrics {
     new Vector3(-innerRadius, 0f, 0.5f * outerRadius),
     new Vector3(0f, 0f, outerRadius)
   };
-	
+
+  public static Vector3 GetFirstCorner(HexDirection direction) {
+    return corners[(int)direction];
+  }
+
+  public static Vector3 GetSecondCorner(HexDirection direction) {
+    return corners[(int)direction + 1];
+  }
+
+  public static Vector3 GetFirstSolidCorner(HexDirection direction) {
+    return corners[(int)direction] * solidFactor;
+  }
+
+  public static Vector3 GetSecondSolidCorner(HexDirection direction) {
+    return corners[(int)direction + 1] * solidFactor;
+  }
+
+  public static Vector3 GetBridge (HexDirection direction) {
+    return (corners[(int)direction] + corners[(int)direction + 1]) * blendFactor;
+  }
 }
