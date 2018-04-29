cd56fb5c500877303fec70941b521fc47ee1960e
touwak <claramuntgi@esat-alumni.com>
2018-04-29 14:35:10 +0100

    min changes

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -33,7 +33,7 @@ public class GameManager : MonoBehaviour
       Destroy(gameObject);
     }
 
-    DontDestroyOnLoad(gameObject);
+    //DontDestroyOnLoad(gameObject);
 		
 		enemies = new List<Enemy>();
     boardScript = GetComponent<BoardManager>();
